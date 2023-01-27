f70d4247c7dcd8cfd92bd2efab7328b65547aeeb
Roberto Valentino Reynoso <68643525+RvRproduct@users.noreply.github.com>
2023-01-26 20:04:31 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,34 @@
 using namespace std;
 
 int main(){
-    Linked linkedList;
-    linkedList.newNode(10, &linkedList);
-    linkedList.newNode(20, &linkedList);
-    linkedList.newNode(30, &linkedList);
-    linkedList.display(&linkedList);
-    linkedList.getHead(&linkedList);
-    linkedList.getTail(&linkedList);
+    Linked *linkedList = new Linked();
+    linkedList->newNode(10, linkedList);
+    linkedList->newNode(20, linkedList);
+    linkedList->newNode(30, linkedList);
+    
+    linkedList->display(linkedList);
+    linkedList->displayHead(linkedList);
+    linkedList->displayTail(linkedList);
+    
+    linkedList->newNodeBegin(50, linkedList);
+    linkedList->newNodeBegin(45, linkedList);
+    linkedList->newNode(25, linkedList);
+    linkedList->displayHead(linkedList);
+    linkedList->display(linkedList);
+    
+    linkedList->newNodeInsert(18, 3, linkedList);
+    linkedList->display(linkedList);
+    linkedList->nodeRemove(2, linkedList);
+    linkedList->display(linkedList);
+    
+    
+    // Be careful with zero since that is the head, which was the original address
+    // since the linkedList started here in main it gets messy, the function will display
+    // the correct output, but after that you won't be able to manipulate the LinkedList
+    // anymore and after this call, delete should be called here.
+    linkedList->nodeRemove(0, linkedList);
+    delete linkedList;
+
     cout << endl;
     return 0;
 }
